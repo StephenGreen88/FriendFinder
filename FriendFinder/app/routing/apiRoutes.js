7fcc2006aef62524fbb6883a374ccaa7cd9dfaed
Stephen Green <stephengreen1028@gmail.com>
2020-01-22 19:14:31 -0700

    Updated the apiRoutes.js file

--- FriendFinder/app/routing/apiRoutes.js
+++ FriendFinder/app/routing/apiRoutes.js
@@ -1,42 +1,63 @@
 // Require necessary dependencies
-var friendMatch = require('../data/friends');
+var friendMatch = require('../data/friends.js');
 
-// API Routes
+//ROUTING
+// Two Routes using the module.exports to make accessible to other files
 module.exports = function (app) {
-    // GET JSON route to display all possible friends
+    // A GET json route to display all possible friends
     app.get('/api/friends', function (req, res) {
         res.json(friendMatch);
     });
-
-    // POST route to handle incoming survey results
+    // A POST route to handle incoming survey results
     app.post('/api/friends', function (req, res) {
+
+        //req.body is available since we're using body-parser middleware
         var newFriend = req.body;
-        // For loop for score
+        //score loop
         for (var i = 0; i < newFriend.scores.length; i++) {
-            if (newFriend.scores[i] == '1 (Yes)') {
+            if (newFriend.scores[i] == "1 (Yes)") {
+
                 newFriend.scores[i] = 1;
-            } else if (newFriend.scores[i] == '3 (No)') {
+            } else if (newFriend.scores[i] == "3 (No)") {
+
                 newFriend.scores[i] = 3;
             } else {
+
                 newFriend.scores[i] = parseInt(newFriend.scores[i]);
             }
         }
 
-        // array for comparison
+        //array for the comparison
         var comparisonArray = [];
 
-        // For loop for comparison
-        for (var i = o; i < comparisonArray.length; i++) {
+        for (var i = 0; i < friendMatch.length; i++) {
+            //Determine the users most compatible friend
+            var comparedFriend = friendMatch[i];
+            //calculate the totaldifference between friends
+            var totalDifference = 0;
+
+            for (var k = 0; k < comparedFriend.scores.length; k++) {
+                //return the absolute value of a number *use abs()method
+                var differenceOneScore = Math.abs(comparedFriend.scores[k] - newFriend.scores[k]);
+                totalDifference += differenceOneScore;
+            }
+
+            comparisonArray[i] = totalDifference;
+        }
+
+        var bestFriendNum = comparisonArray[0];
+        var bestFriendI = 0;
+
+        for (var i = 1; i < comparisonArray.length; i++) {
             if (comparisonArray[i] < bestFriendNum) {
                 bestFriendNum = comparisonArray[i];
                 bestFriendI = i;
             }
         }
 
-        // Push new friend 
+        //push new friend
         friendMatch.push(newFriend);
-        // JSON bestFriend to the current friend match array
+        //json bf to the current friend match array
         res.json(friendMatch[bestFriendI]);
-
     });
 };
